6888d72fc970ade3bb51607a6422e60c4dac2a35
jessjimenez <jleticiaj@yahoo.com>
2015-11-18 23:51:46 -0600

    Update p2.s

--- p2.s
+++ p2.s
@@ -1,46 +1,27 @@
+/******************************************************************************
+*Program 2: Count Partitions
+*Jessica Jimenez
+******************************************************************************/
  
     .global main
     .func main
    
 main:
-    BL  _prompt             @ branch to prompt procedure with return
-    BL  _scanf              @ branch to scan procedure with return
+    BL  _scanf              @ scan n
     MOV R4, R0              @ store n in R4
-    MOV R1, R0              @ pass n to factorial procedure
-    BL  _fact               @ branch to factorial procedure with return
+    BL  _scanf              @ scan m
+    MOV R5, R0              @ store m in R5
+    MOV R1, R4              @ pass n to count partitions procedure
+    MOV R2, R5              @ pass m to count partitions procedure
+    BL  _cnt_part           @ branch to count partitions procedure with return
     MOV R1, R4              @ pass n to printf procedure
-    MOV R2, R0              @ pass result to printf procedure
-    BL  _printf             @ branch to print procedure with return
-    B   _exit               @ branch to exit procedure with no return
-   
-_exit:  
-    MOV R7, #4              @ write syscall, 4
-    MOV R0, #1              @ output stream to monitor, 1
-    MOV R2, #21             @ print string length
-    LDR R1, =exit_str       @ string at label exit_str:
-    SWI 0                   @ execute syscall
-    MOV R7, #1              @ terminate syscall, 1
-    SWI 0                   @ execute syscall
- 
-_prompt:
-    PUSH {R1}               @ backup register value
-    PUSH {R2}               @ backup register value
-    PUSH {R7}               @ backup register value
-    MOV R7, #4              @ write syscall, 4
-    MOV R0, #1              @ output stream to monitor, 1
-    MOV R2, #26             @ print string length
-    LDR R1, =prompt_str     @ string at label prompt_str:
-    SWI 0                   @ execute syscall
-    POP {R7}                @ restore register value
-    POP {R2}                @ restore register value
-    POP {R1}                @ restore register value
-    MOV PC, LR              @ return
+    MOV R2, R5              @ m to print f
+    MOV R0, R0              @ pass result to printf procedure
+    B  _printf              @ branch to print procedure without return
        
 _printf:
     PUSH {LR}               @ store the return address
     LDR R0, =printf_str     @ R0 contains formatted string address
-    @MOV R1, R1              @ R1 contains printf argument 1 (redundant line)
-    @MOV R2, R2              @ R2 contains printf argument 2 (redundant line)
     BL printf               @ call printf
     POP {PC}                @ restore the stack pointer and return
    
@@ -56,22 +37,40 @@ _scanf:
     POP {R1}                @ restore register value
     POP {PC}                @ restore the stack pointer and return
  
-_fact:
+_cnt_part:
     PUSH {LR}               @ store the return address
-    CMP R1, #1              @ compare the input argument to 1
-    MOVEQ R0, #1            @ set return value to 1 if equal
+    CMP R1, #0              @ compare the input argument to 1
+    MOVEQ R0, #1            @ set return value to 1 if n equals 0
     POPEQ {PC}              @ restore stack pointer and return if equal
    
-    PUSH {R1}               @ backup input argument value  
-    SUB R1, R1, #1          @ decrement the input argument
-    BL _fact                @ compute fact(n-1)
-    POP {R1}                @ restore input argument
-    MUL R0, R0, R1          @ compute fact(n-1)*n
-    POP  {PC}               @ restore the stack pointer and return
+    MOVLT R0, #0            @ else if n<0, set return value to 0
+    POPLT {PC}              @ restore stack pointer and return if less than
+    
+    CMP R2, #0              @ compare the input argument to 0
+    MOVEQ R0, #0            @ set return value to 0 if n <  0
+    POPEQ {PC}              @ restore stack pointer and return if equal
+    @---else
+    PUSH {LR}               @ store the return address
+    PUSH {R1}               @ backup register value
+    PUSH {R2}               @ backup regsiter value
+    SUB R1, R1, R2          @ r1-r2=r1
+    BL _cnt_part            @ call _cnt_part
+    POP {R2}                @ restore register value
+    POP {R1}                @ restore register value
+    PUSH {R0}               @ backup register value
+    POP {R7}                @ restore register value
+    @PUSH {R2}              @ back up what we're messing up but we don't need it later so yeah
+    
+    SUB R2, R2, #1          @ r2-1=r2
+    BL _cnt_part            @ call _cnt_part
+    @POP {R2}               @ samesies
+    PUSH {R0}               @ backup register value
+    POP (R8}                @ restore register value
+    ADD R0, R7, R8          @ r7+r8=r0
+    POP {PC}                @ restore register value
+    
  
 .data
 number:         .word       0
 format_str:     .asciz      "%d"
-prompt_str:     .asciz      "Enter a positive number: "
-printf_str:     .asciz      "%d! = %d\n"
-exit_str:       .ascii      "Terminating program.\n"
+printf_str:     .asciz      "%there are %d partitions of %d using integers up to %d\n\n"
